35d473e62a57f8020546a0b91868c0ac2a1505c6
brianbentancourt <brianbentancourt9@gmail.com>
2019-09-05 20:46:35 -0300

    getInitialProps get channels

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,69 @@
+import 'isomorphic-fetch'
+import Link from 'next/link'
+
 export default class extends React.Component {
+
+  static async getInitialProps() {
+    let req = await fetch('https://api.audioboom.com/channels/recommended')
+    let { body: channels } = await req.json()
+    return { channels }
+  }
+
   render() {
+    const { channels } = this.props
+
     return <div>
-      <h1>¡Hola Platzi!</h1>
-      <p>Bienvenidos al curso de Next.JS</p>
-      <img src='/static/logo.png' alt="Logo" />
+      <header>Podcasts</header>
+
+      <div className="channels">
+        { channels.map((channel) => (
+          <a className="channel" key={ channel.id }>
+            <img src={ channel.urls.logo_image.original } alt=""/>
+            <h2>{ channel.title }</h2>
+          </a>
+        )) }
+      </div>
 
       <style jsx>{`
-        h1 {
-          color: red;
+        header {
+          color: #fff;
+          background: #8756ca;
+          padding: 15px;
+          text-align: center;
         }
-        img {
-          max-width:50%;
-          display:block;
-          margin: 0 auto;
+        .channels {
+          display: grid;
+          grid-gap: 15px;
+          padding: 15px;
+          grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
+        }
+        a.channel {
+          display: block;
+          margin-bottom: 0.5em;
+          color: #333;
+          text-decoration: none;
+        }
+        .channel img {
+          border-radius: 3px;
+          box-shadow: 0px 2px 6px rgba(0,0,0,0.15);
+          width: 100%;
+        }
+        h2 {
+          padding: 5px;
+          font-size: 0.9em;
+          font-weight: 600;
+          margin: 0;
+          text-align: center;
         }
       `}</style>
 
+      <style jsx global>{`
+        body {
+          margin: 0;
+          font-family: system-ui;
+          background: white;
+        }
+      `}</style>
     </div>
   }
 }
