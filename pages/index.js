e1b09dfe48220701333e84e49d08cf5b434d56ab
brianbentancourt <brianbentancourt9@gmail.com>
2019-09-05 20:38:49 -0300

    style jsx

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,19 @@ export default class extends React.Component {
     return <div>
       <h1>¡Hola Platzi!</h1>
       <p>Bienvenidos al curso de Next.JS</p>
+      <img src='/static/logo.png' alt="Logo" />
+
+      <style jsx>{`
+        h1 {
+          color: red;
+        }
+        img {
+          max-width:50%;
+          display:block;
+          margin: 0 auto;
+        }
+      `}</style>
+
     </div>
   }
 }
